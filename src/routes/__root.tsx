f0fc95f6cee9697dac8468e7d96e4768be268b05
austin-davis1 <austindd@outlook.com>
2025-08-28 19:19:20 -0500

    don't need the side nav anymore :(

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,6 +1,5 @@
 import { Outlet, createRootRoute } from "@tanstack/react-router"
 import Landing from "../components/Landing"
-import SideNav from "../components/Sidenav"
 
 export const Route = createRootRoute({
   component: RootComponent,
@@ -10,8 +9,7 @@ function RootComponent() {
   return (
     <>
       <Landing />
-      <SideNav />
-      <div className="ml-[var(--sidebar-width)] px-12 py-2">
+      <div className="px-12 py-2">
         <Outlet />
       </div>
     </>
